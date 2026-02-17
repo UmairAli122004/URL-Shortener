af003f6ada2c54f9e84c82f572f03579a39afc0c
Umair Ali <aliumair7379@gmail.com>
2026-02-17 16:52:57 +0530

    Update ApplicationProperties.java

--- src/main/java/com/UrlShortener/ApplicationProperties.java
+++ src/main/java/com/UrlShortener/ApplicationProperties.java
@@ -6,19 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.validation.annotation.Validated;
-/*
-* @ConfigurationProperties is used to bind values from application.properties (or YAML) into a Java class.
-        It automatically maps:
-                    property keys → Java fields
-                    without writing @Value again and again.
-✅ 2. What does prefix = "app" mean?
-        It means:
-                “Bind all properties that start with app. into this Java class.”
-        Example in application.properties:
-                app.base-url=http://localhost:8080
-                app.default-expiry-in-days=30
-                app.validate-original-url=true
-*/
 
 @ConfigurationProperties(prefix = "app")
 @Validated
